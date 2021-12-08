773b7f02a53795d8f201e0adbe974f36264b97c1
bosoek <qhtjr11567@gmail.com>
2021-12-08 13:38:11 +0900

    2021.12.08 강보석 notice java 재커밋

--- team1/src/main/java/com/myspring/baroip/notice/dao/NoticeDAO.java
+++ team1/src/main/java/com/myspring/baroip/notice/dao/NoticeDAO.java
@@ -1,19 +1,5 @@
 package com.myspring.baroip.notice.dao;
 
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.dao.DataAccessException;
-
-import com.myspring.baroip.notice.vo.NoticeVO;
-
 public interface NoticeDAO {
-	public List selectAllArticlesList() throws DataAccessException;
-	public int insertNewArticle(Map articleMap) throws DataAccessException;
-	public NoticeVO selectArticle(String articleNO) throws DataAccessException;
-	public void updateArticle(Map articleMap) throws DataAccessException;
-	public void deleteArticle(int articleNO) throws DataAccessException;
-}
-
 
 }
