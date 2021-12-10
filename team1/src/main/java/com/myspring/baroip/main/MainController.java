e1a43bbe1664a98679d5a3c62ac8f98a69831a27
yoon92kr <yoon92_kr@naver.com>
2021-12-10 17:49:46 +0900

    2021.12.10 윤상현 ▶ 최종 commit

--- team1/src/main/java/com/myspring/baroip/main/MainController.java
+++ team1/src/main/java/com/myspring/baroip/main/MainController.java
@@ -7,6 +7,8 @@ package com.myspring.baroip.main;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,7 +16,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class MainController {
-
+	private static final Logger logger = LoggerFactory.getLogger(ExceptionController.class);
 	@RequestMapping(value= "/main.do" ,method={RequestMethod.POST,RequestMethod.GET})
 	public ModelAndView main(HttpServletRequest request, HttpServletResponse response) throws Exception{
 		// HttpSession session;
@@ -24,8 +26,6 @@ public class MainController {
 		
 		return mav;
 	}
-	
-	
 	
 	
 
