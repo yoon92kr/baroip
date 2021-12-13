881987a486df3e174bf75898da4e4cd078350652
yoon92kr <yoon92_kr@naver.com>
2021-12-13 17:24:03 +0900

    2021.12.13 윤상현 ▶ 중간커밋_product 작업중

--- team1/src/main/java/com/myspring/baroip/image/service/ImageServiceImpl.java
+++ team1/src/main/java/com/myspring/baroip/image/service/ImageServiceImpl.java
@@ -2,22 +2,38 @@
 
 package com.myspring.baroip.image.service;
 
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.myspring.baroip.image.dao.ImageDAO;
 import com.myspring.baroip.image.vo.ImageVO;
 
 @Service("imageService")
+@Transactional(propagation=Propagation.REQUIRED)
 public class ImageServiceImpl implements ImageService {
 	
 	@Autowired
 	private ImageDAO imageDAO;
 
+	@Override
 	public String addImageFile(ImageVO imageVO) throws Exception {
 		
 		String imageName = imageDAO.insertImageFile(imageVO);
 		
 		return imageName;
+	}
+	
+	// option 에는 match_id / image_category 가 대입되어야 한다.
+	@Override
+	public ImageVO selectProductImage(Map<String, String> option) throws Exception {
+		ImageVO image = imageDAO.selectProductImages(option);
+		
+		return image;
+		
+		
 	}
 }
