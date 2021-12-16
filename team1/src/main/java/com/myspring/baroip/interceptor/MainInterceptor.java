721cb951a1e5156c1f3d9540bf1027c30880abd7
yoon92kr <yoon92_kr@naver.com>
2021-12-16 17:11:13 +0900

    2021.12.16 윤상현 ▶ main 수정

--- team1/src/main/java/com/myspring/baroip/interceptor/MainInterceptor.java
+++ team1/src/main/java/com/myspring/baroip/interceptor/MainInterceptor.java
@@ -25,27 +25,27 @@ public class MainInterceptor extends HandlerInterceptorAdapter {
 			setHeader(request, viewName);
 			// admin 접근시 관리자 rank 유효성 검사
 
-			if (viewName.contains("admin")) {
-
-				if (session.getAttribute("userInfo") != null) {
-					UserVO userVO = (UserVO) session.getAttribute("userInfo");
-
-					if (Integer.parseInt(userVO.getUser_rank()) > 1) {
-						request.setAttribute("viewName", viewName);
-					}
-
-					else {
-						request.setAttribute("viewName", "redirect:/main.do");
-					}
-				} else {
-					request.setAttribute("viewName", "redirect:/main.do");
-				}
-			} else {
-
-				request.setAttribute("viewName", viewName);
-
-			}
-
+//			if (viewName.contains("admin")) {
+//
+//				if (session.getAttribute("userInfo") != null) {
+//					UserVO userVO = (UserVO) session.getAttribute("userInfo");
+//
+//					if (Integer.parseInt(userVO.getUser_rank()) > 1) {
+//						request.setAttribute("viewName", viewName);
+//					}
+//
+//					else {
+//						request.setAttribute("viewName", "redirect:/main.do");
+//					}
+//				} else {
+//					request.setAttribute("viewName", "redirect:/main.do");
+//				}
+//			} else {
+//
+//				request.setAttribute("viewName", viewName);
+//
+//			}
+			request.setAttribute("viewName", viewName);	
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
