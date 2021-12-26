ac771127209decf058aa41af5e086afdc0116f53
yoon92kr <yoon92_kr@naver.com>
2021-12-26 23:39:10 +0900

    2021.12.26 윤상현 ▶ 코드 가독성 수정_adminProductDAO

--- team1/src/main/java/com/myspring/baroip/adminProduct/dao/AdminProductDAO.java
+++ team1/src/main/java/com/myspring/baroip/adminProduct/dao/AdminProductDAO.java
@@ -10,13 +10,19 @@ import org.springframework.dao.DataAccessException;
 import com.myspring.baroip.product.vo.ProductVO;
 
 public interface AdminProductDAO {
+	
+	// 상품 등록 DAO, 반환값 == 등록된 product_id
 	public String insertProduct(ProductVO productVO) throws DataAccessException;
 	
+	// 상품 수량 변경 DAO
 	public void updateAmount(Map<String, String> option) throws DataAccessException;
 	
+	// 상품 삭제 DAO
 	public void deleteProduct(String product_id) throws DataAccessException;
 	
+	// 상품 수정 DAO
 	public void updateProduct(ProductVO productVO) throws DataAccessException;
 	
+	// 조회 조건에 따른 상품 리스트 조회 DAO
 	public List<ProductVO> productListToOption( Map<String, String> option) throws DataAccessException;
 }
