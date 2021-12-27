38144d54b3dc3131081bd388f817f5f16d9d1f9b
amang33 <rjsgml4345@gmail.com>
2021-12-28 00:51:41 +0900

    2021.12.28 한건희 ▶ cart 수정 중간 커밋

--- team1/src/main/java/com/myspring/baroip/cart/controller/CartControllerImpl.java
+++ team1/src/main/java/com/myspring/baroip/cart/controller/CartControllerImpl.java
@@ -11,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.myspring.baroip.cart.service.CartService;
@@ -29,6 +28,7 @@ public class CartControllerImpl implements CartController{
 	
 	
 	// 장바구니 페이지
+	@SuppressWarnings("unlikely-arg-type")
 	@Override
 	@RequestMapping(value= "/cartList.do" ,method={RequestMethod.POST,RequestMethod.GET})
 	public ModelAndView mycartList(HttpServletRequest request, 
@@ -41,9 +41,9 @@ public class CartControllerImpl implements CartController{
 		cartVO.setUser_id(user_id);
 		Map<String, Map<String, Map<String, Object>>> userCartListInfo = cartService.myCartList(cartVO);
 //		System.out.println("cartcontroller(userCartListInfo.user_id : " + userCartListInfo);
-//		System.out.println("cartcontroller(userCartListInfo) : " + userCartListInfo.size());
+		System.out.println("cartcontroller(userCartListInfo) : " + userCartListInfo.size());
 //		System.out.println("cartController : " + userCartListInfo.get("myCartList0").get("product").get("productVO"));
-//		System.out.println("cartController(cart_count) : " + userCartListInfo.get("myCartList1").get(cartVO.getCart_count()));
+		System.out.println("cartController(cart_count) : " + userCartListInfo.get("myCartList1").get("cart").get(cartVO.getCart_count()));
 		mav.addObject("userCartListInfo", userCartListInfo);
 		mav.setViewName(viewName);
 		return mav;
@@ -53,20 +53,20 @@ public class CartControllerImpl implements CartController{
 //	상품 상세 페이지 > 장바구니 담기 기능
 	@Override
 	@RequestMapping(value= "/addProductInCart.do" ,method={RequestMethod.POST,RequestMethod.GET})
-	public ModelAndView addProductInCart(@RequestParam("cartVO") CartVO cartVO, 
+	public CartVO addProductInCart(@RequestParam("cartVO") CartVO cartVO, 
 			HttpServletRequest request, 
 			HttpServletResponse response) throws Exception {
-		ModelAndView mav = new ModelAndView();
-		String viewName = (String)request.getAttribute("viewName");
+//		ModelAndView mav = new ModelAndView();
+//		String viewName = (String)request.getAttribute("viewName");
 		HttpSession session=request.getSession();
 		UserVO userVO = (UserVO)session.getAttribute("userInfo");
 		String user_id = userVO.getUser_id();
 		cartVO.setUser_id(user_id);
 		cartService.addProductInCart(cartVO);
 		System.out.println("cartController(addProductInCart) : " + cartVO.getProduct_id());
-		mav.addObject(cartVO);
-		mav.setViewName("/product/productDetail.do");
-		return mav;
+//		mav.addObject(cartVO);
+//		mav.setViewName("/product/productDetail.do");
+		return cartVO;
 	}
 	
 }
